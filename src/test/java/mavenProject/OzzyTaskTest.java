eba2457fb4ae11289794cf534addf956ce4da71c
Aydana9008 <aydana_9008@yahoo.com>
2018-12-16 19:11:09 -0500

    updated POM file

--- src/test/java/mavenProject/OzzyTaskTest.java
+++ src/test/java/mavenProject/OzzyTaskTest.java
@@ -2,7 +2,6 @@ package mavenProject;
 
 import org.testng.annotations.Test;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
