00f18c233b7ae4e6aa2faf34629f906c1a351729
Aydana9008 <aydana_9008@yahoo.com>
2018-12-16 19:20:32 -0500

    Updates

--- src/test/java/mavenProject/checkTest.java
+++ src/test/java/mavenProject/checkTest.java
@@ -4,9 +4,9 @@ import org.junit.Test;
 
 public class checkTest {
 
-//	@Test
-//	public void print() {
-//		System.out.println("Maven are you listening to me??");
-//	}
+	@Test
+	public void print() {
+		System.out.println("Maven are you listening to me??");
+	}
 
 }
